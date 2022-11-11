f232ed1ceeee5440aa161f6a36c73c0ec68f1e85
Dulcie <dulcie@gravitywell.co.uk>
2022-11-11 16:40:59 +0000

    :lobster: Hackery

---
+++ packages/node/auth/adapter/oidc.js
@@ -0,0 +1,59 @@
+import { generators } from "openid-client";
+import { useCookie, useDomainName, useFormData, usePath, } from "../../api/index.js";
+import { createAdapter } from "./adapter.js";
+export const OidcAdapter = /* @__PURE__ */ createAdapter((config) => {
+    return async function () {
+        const [step] = usePath().slice(-1);
+        const callback = "https://" +
+            [useDomainName(), ...usePath().slice(0, -1), "callback"].join("/");
+        const client = new config.issuer.Client({
+            client_id: config.clientID,
+            redirect_uris: [callback],
+            response_types: ["id_token"],
+        });
+        if (step === "authorize") {
+            const nonce = generators.nonce();
+            const state = generators.state();
+            const url = client.authorizationUrl({
+                scope: config.scope,
+                response_mode: "form_post",
+                nonce,
+                state,
+            });
+            const expires = new Date(Date.now() + 1000 * 30).toUTCString();
+            return {
+                statusCode: 302,
+                cookies: [
+                    `auth-nonce=${nonce}; HttpOnly; expires=${expires}`,
+                    `auth-state=${state}; HttpOnly; expires=${expires}`,
+                ],
+                headers: {
+                    location: url,
+                },
+            };
+        }
+        if (step === "callback") {
+            const form = useFormData();
+            if (!form)
+                throw new Error("Missing body");
+            const params = Object.fromEntries(form.entries());
+            const nonce = useCookie("auth-nonce");
+            const state = useCookie("auth-state");
+            // ! Added by GW to fix "checks.state argument is missing" error on first Google auth call
+            // ! This redirects back to the authorize step, which seems to correctly apply the nonce & state cookies so this works
+            if (!nonce || !state)
+                return {
+                    statusCode: 302,
+                    headers: {
+                        location: "https://" + [useDomainName(), ...usePath().slice(0, -1), "authorize"].join("/")
+                    }
+                };
+            const tokenset = await client.callback(callback, params, {
+                nonce,
+                state,
+            });
+            return config.onSuccess(tokenset, client);
+        }
+        throw new Error("Invalid auth request");
+    };
+});
